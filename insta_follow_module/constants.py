70495c8ce6789d6d0931f53fbdeae1e011ee86f4
Sanket <sanket26k@gmail.com>
2022-09-30 07:52:44 +0530

    Removing unused constants

--- insta_follow_module/constants.py
+++ insta_follow_module/constants.py
@@ -8,19 +8,4 @@ follow_xpath = "//*[text()='Follow']"
 unfollow_xpath = "//*[text()='Following']"
 unrequest_xpath = "//*[text()='Requested']"
 unfollow_button_xpath = "//button[contains(text(), 'Unfollow')]"
-search_class_name = "_aauy"
-comments_class_name = "_a9ym"
-
-
-# list of #hastags to search for
-hashtags = [
-    "FLStudio",
-    "Musician",
-    "Hip-Hop",
-    "RnB"
-]
-
 fl_daily_user = "flstudiodaily"
-
-def user_xpath(n):
-    return '/html/body/div[1]/div/div/div/div[1]/div/div/div/div[1]/section/main/div[1]/div[1]/article/div/div[2]/div/div[2]/div[1]/ul/ul[' + str(n) + ']/div/li/div/div/div[2]/h3/div/span/a'
